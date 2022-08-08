8b6f35bf9d872be4d16764711c75a21da2971ab9
huna <2496337440@qq.com>
2022-08-08 16:01:29 +0800

    全屏和国际化

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import React, { Component } from 'react'
+import English from './page/English'
  
 export default class App extends Component {
   render() {
     return (
       <>
-   
+
       </>
     )
   }
