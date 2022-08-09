f92567dcf0e8fd6b10e70bd4b75415e88cd915c5
Drasas <50647358@qq.com>
2022-08-09 15:20:20 +0800

    “上传角色管理页面”

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,8 @@ import thunk from 'redux-thunk'
 import {legacy_createStore as createStore, applyMiddleware,compose, combineReducers} from 'redux'
 import collapseReducer from './reducer/collaspe_reducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
 const allReducers = combineReducers({
     collapse:collapseReducer
 })
-export default createStore(allReducers, composeEnhancers(applyMiddleware(thunk)));
+export default createStore(allReducers, applyMiddleware(thunk));
