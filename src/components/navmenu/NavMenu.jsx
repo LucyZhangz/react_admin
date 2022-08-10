d7cf158e8e9213689ea9db6299426d09c94a59e5
Drasas <50647358@qq.com>
2022-08-10 09:31:02 +0800

    侧边菜单栏

--- src/components/navmenu/NavMenu.jsx
+++ src/components/navmenu/NavMenu.jsx
@@ -4,13 +4,39 @@ import { Layout, Menu } from "antd";
 import style from "./nav.module.less";
 import { createCollapseAction } from "../../store/actions/collaspe_action";
 import { connect } from "react-redux";
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 const { Sider } = Layout;
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+};
+
+const items = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
+    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
+  ]),
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+  ]),
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+];
+
+const onClick = (e) => {
+  console.log('click ', e);
+};
 function NavMenu(props) {
   return (
     <Sider style={{ height: "100vh" }} trigger={null} collapsible collapsed={props.collapsed}>
@@ -22,27 +48,13 @@ function NavMenu(props) {
         )}
       </div>
       <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={["1"]}
-        items={[
-          {
-            key: "1",
-            icon: <UserOutlined />,
-            label: "nav 1",
-          },
-          {
-            key: "2",
-            icon: <VideoCameraOutlined />,
-            label: "nav 2",
-          },
-          {
-            key: "3",
-            icon: <UploadOutlined />,
-            label: "nav 3",
-          },
-        ]}
-      />
+      onClick={onClick}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+      items={items}
+      theme="dark"
+    />
     </Sider>
   );
 }
